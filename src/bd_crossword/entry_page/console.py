b5ed4a658bf3b657e6cae2e371854d335adf82b0
Chris Denney <chris.j.denney@gmail.com>
2023-05-23 21:06:39 +0100

    add parse functionality to console

--- src/bd_crossword/entry_page/console.py
+++ src/bd_crossword/entry_page/console.py
@@ -5,6 +5,7 @@ from . import __version__
 from bd_crossword.common import crossword_index
 
 from . import entry_page_getter
+from . import entry_page_parser
 import logging
 
 # import requests
@@ -21,13 +22,14 @@ logging.basicConfig(
 )
 
 
+# default=datetime.now().strftime("%Y-%m-%d"),
 @click.command()
 @click.option(
     "--page-date-string",
     "-p",
-    default=datetime.now().strftime("%Y-%m-%d"),
-    help="The page date of the days to get",
-    metavar="YYYY-MM-DD",
+    default="ALL",
+    help="The page date of the day to get",
+    metavar="YYYY-MM-DD or ALL",
     show_default=True,
 )
 @click.option(
@@ -49,13 +51,32 @@ logging.basicConfig(
 def main(page_date_string, dump, force_download):
     """CLI for downloading BD entry pages"""
 
-    click.echo(f"Getting entry pages starting at {page_date_string}")
-    page_date = date.fromisoformat(page_date_string)
-    logger.debug("page_date : %s", str(page_date))
-
     database_file = "bd_crossword.db"
     db = crossword_index.CrosswordIndex(filename=database_file)
 
+    if page_date_string == "ALL":
+        logger.info("No page date specified.")
+        get_all_entry_pages(db)
+    else:
+        click.echo(f"Getting entry pages starting at {page_date_string}")
+        page_date = date.fromisoformat(page_date_string)
+        logger.debug("page_date : %s", str(page_date))
+        get_entry_page(page_date, db)
+
+def get_entry_page(page_date, db):
+    index_entry = db.retrieve_index_entry_for_date(page_date)
+    logger.debug(f"{index_entry=}")
+    page_html = entry_page_getter.get_entry_page(index_entry.title, index_entry.url)
+    crossword_clues = entry_page_parser.parse_entry_page(page_html)
+    
+    across_clues = crossword_clues["across"]
+    down_clues = crossword_clues["down"]
+
+    for clue_id, clue in across_clues.items():
+        logger.debug(f"{clue_id=}, {clue=}")
+
+
+def get_all_entry_pages(db):
     for title, url in db.retrieve_all_urls():
         print(f"{url=}")
         entry_page_getter.get_entry_page(title, url)
