ba59540744602de84d0ef23bc4fe04723d0da0c1
Chris Denney <chris.j.denney@gmail.com>
2023-06-05 22:37:49 +0100

    refactor only

--- src/bd_crossword/entry_page/entry_page_fill_grid.py
+++ src/bd_crossword/entry_page/entry_page_fill_grid.py
@@ -18,37 +18,43 @@ class FillGrid:
         self.recurse_count = recurse_count
         self.stop_after = stop_after
 
+
+    def fill_clue(self):
+        item = self.clues.pop(0)
+        word, clue_id, direction = item.actual_solution, item.clue_id, item.direction
+        for try_num in range(35):
+            self.try_count += 1
+            logger.debug("%s%s %s, r%sc%s, try is %s", clue_id, direction, word, self.row, self.col, try_num)
+
+            grid_savepoint = self.deep_copy()
+            if self.grid.write_direction(self.row, self.col, word, direction, clue_id):
+                logger.debug("before recurse call grid is %s", self.grid.text_grid())
+                if word == self.stop_after:
+                    return self
+
+                if self.fill_grid() is not False:
+                    logger.debug("returned fill_grid back to us with content %s", self.grid.text_grid())
+                    return self
+
+                logger.debug("<<<<<<<< fill_grid returned False, so we restore the grid to %s", grid_savepoint.grid.text_grid())
+                self.restore_from_deep_copy(grid_savepoint)
+
+            self.move_to_next_square()
+
+        logger.debug(
+            "no more tries left, row is %s, col is %s, word is %s, direction is %s, grid is %s",
+            self.row, self.col, word, direction, self.grid.text_grid()
+        )
+        return False
+
     def fill_grid(self):
         self.recurse_count += 1
         logger.debug("fill_grid called at depth %s, clues left are %s", self.recurse_count, len(self.clues))
-        while len(self.clues) > 0:
-            item = self.clues.pop(0)
-            word, clue_id, direction = item.actual_solution, item.clue_id, item.direction
-            for try_num in range(35):
-                self.try_count += 1
-                logger.debug("%s%s %s, r%sc%s, try is %s", clue_id, direction, word, self.row, self.col, try_num)
+        if len(self.clues) > 0:
+            if self.fill_clue() is False:
+                logger.debug("fill_clue returned False, so we return False")
+                return False
 
-                grid_savepoint = self.deep_copy()
-                if self.grid.write_direction(self.row, self.col, word, direction, clue_id):
-                    logger.debug("now grid is %s", self.grid.text_grid())
-                    if self.fill_grid() or word == self.stop_after:
-                        logger.debug("returned fill_grid back to us with content %s", self.grid.text_grid())
-                        return self
-                    else:
-                        logger.debug("<<<<<<<< fill_grid returned False, so we restore the grid to %s", grid_savepoint.grid.text_grid())
-                        self.grid = grid_savepoint.grid.deep_copy()
-                        self.clues = list(grid_savepoint.clues)
-                        self.row = grid_savepoint.row
-                        self.col = grid_savepoint.col
-
-
-                self.move_to_next_square()
-
-            logger.debug(
-                "no more tries left, row is %s, col is %s, word is %s, direction is %s, grid is %s",
-                self.row, self.col, word, direction, self.grid.text_grid()
-            )
-            return False
         return self        
                 # return self.fill_grid_starting_with(row, col + 1, current_clue_index + 1)
         # return self.grid
@@ -66,6 +72,14 @@ class FillGrid:
     def deep_copy(self):
         new_grid = self.grid.deep_copy()
         return FillGrid(list(self.clues), new_grid, self.row, self.col, self.try_count, self.recurse_count, self.stop_after)
+    
+    def restore_from_deep_copy(self, deep_copy):
+        self.grid = deep_copy.grid.deep_copy()
+        self.clues = list(deep_copy.clues)
+        self.row = deep_copy.row
+        self.col = deep_copy.col
+        self.try_count = deep_copy.try_count
+
 
     def list_clues(self):
         for clue in self.clues:
